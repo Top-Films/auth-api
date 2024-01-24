ec7e287a1713ea2cf3828ed7e1b243ed0b2f39dd
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-01-24 18:52:08 -0500

    config seperate file routes for getting/setting name

---
+++ src/config.ts
@@ -0,0 +1,82 @@
+import dotenv from 'dotenv';
+import Dashboard from 'supertokens-node/recipe/dashboard';
+import Session from 'supertokens-node/recipe/session';
+import ThirdPartyEmailPassword from 'supertokens-node/recipe/thirdpartyemailpassword';
+import UserMetadata from 'supertokens-node/recipe/usermetadata';
+import UserRoles from 'supertokens-node/recipe/userroles';
+import { TypeInput } from 'supertokens-node/types';
+
+dotenv.config();
+
+/* Configuration */
+const applicationName = 'Top Films';
+
+// Third party provider ids
+const googleProviderId: string = 'google';
+const twitterProviderId: string = 'twitter';
+const discordProviderId: string = 'discord';
+
+// Domains based on env
+export const websiteDomain: string = process.env.FRONTEND_URL ?? 'http://localhost:3000';
+export const apiDomain: string = process.env.AUTHENTICATION_API_URL ?? 'http://localhost:3001';
+export const superTokensCoreUrl: string = process.env.SUPER_TOKENS_CORE_URL ?? 'http://localhost:3567';
+
+// Configuration for super tokens
+export const SuperTokensConfig: TypeInput = {
+	supertokens: {
+		connectionURI: superTokensCoreUrl,
+		apiKey: process.env.SUPER_TOKENS_CORE_API_KEY
+	},
+	appInfo: {
+		appName: applicationName,
+		apiDomain,
+		websiteDomain
+	},
+	recipeList: [
+		ThirdPartyEmailPassword.init({
+			providers: [
+				{
+					config: {
+						thirdPartyId: googleProviderId,
+						clients: [
+							{
+								clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+								clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+							}
+						]
+					}
+				},
+				{
+					config: {
+						thirdPartyId: twitterProviderId,
+						clients: [
+							{
+								clientId: process.env.TWITTER_CLIENT_ID ?? '',
+								clientSecret: process.env.TWITTER_CLIENT_SECRET ?? ''
+							}
+						]
+					}
+				},
+				{
+					config: {
+						thirdPartyId: discordProviderId,
+						clients: [
+							{
+								clientId: process.env.DISCORD_CLIENT_ID ?? '',
+								clientSecret: process.env.DISCORD_CLIENT_SECRET ?? ''
+							}
+						]
+					}
+				}
+
+			]
+		}),
+		// Use bearer token instead of cookies
+		Session.init({
+			getTokenTransferMethod: () => 'header'
+		}),
+		Dashboard.init(),
+		UserRoles.init(),
+		UserMetadata.init()
+	]
+};
--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,21 @@
-import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
 import supertokens from 'supertokens-node';
-import { middleware, errorHandler } from 'supertokens-node/framework/express';
+import { errorHandler, middleware } from 'supertokens-node/framework/express';
 import Multitenancy from 'supertokens-node/recipe/multitenancy';
-import ThirdPartyEmailPassword from 'supertokens-node/recipe/thirdpartyemailpassword';
-import Session from 'supertokens-node/recipe/session';
-import { TypeInput } from 'supertokens-node/types';
-import Dashboard from 'supertokens-node/recipe/dashboard';
-import UserRoles from 'supertokens-node/recipe/userroles';
-import dotenv from 'dotenv'; 
+import { verifySession } from 'supertokens-node/recipe/session/framework/express';
+import UserMetadata from 'supertokens-node/recipe/usermetadata';
+import { SuperTokensConfig, apiDomain, superTokensCoreUrl, websiteDomain } from './config';
+
+import bodyParser = require('body-parser');
 
 dotenv.config();
-
-/* Configuration */
-const applicationName = 'Top Films';
-
-// Third party provider ids
-const googleProviderId: string = 'google';
-const twitterProviderId: string = 'twitter';
-const discordProviderId: string = 'discord';
-
-// Domains based on env
-const apiDomain: string = process.env.AUTHENTICATION_API_URL ?? 'http://localhost:3001';
-const superTokensCoreUrl: string = process.env.SUPER_TOKENS_CORE_URL ?? 'http://localhost:3567';
-const websiteDomain: string = process.env.FRONTEND_URL ?? 'http://localhost:3000';
 
 // Debug logs for env
 console.log(`Auth API Domain: ${apiDomain}`);
 console.log(`Super Tokens Domain: ${superTokensCoreUrl}`);
 console.log(`Frontend Domain: ${websiteDomain}`);
-
-// Configuration for super tokens
-const SuperTokensConfig: TypeInput = {
-	supertokens: {
-		connectionURI: superTokensCoreUrl,
-		apiKey: process.env.SUPER_TOKENS_CORE_API_KEY
-	},
-	appInfo: {
-		appName: applicationName,
-		apiDomain,
-		websiteDomain
-	},
-	recipeList: [
-		ThirdPartyEmailPassword.init({
-			providers: [
-				{
-					config: {
-						thirdPartyId: googleProviderId,
-						clients: [
-							{
-								clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-								clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
-							}
-						]
-					}
-				},
-				{
-					config: {
-						thirdPartyId: twitterProviderId,
-						clients: [
-							{
-								clientId: process.env.TWITTER_CLIENT_ID ?? '',
-								clientSecret: process.env.TWITTER_CLIENT_SECRET ?? ''
-							}
-						]
-					}
-				},
-				{
-					config: {
-						thirdPartyId: discordProviderId,
-						clients: [
-							{
-								clientId: process.env.DISCORD_CLIENT_ID ?? '',
-								clientSecret: process.env.DISCORD_CLIENT_SECRET ?? ''
-							}
-						]
-					}
-				}
-
-			]
-		}),
-		// Use bearer token instead of cookies
-		Session.init({
-			getTokenTransferMethod: () => 'header'
-		}),
-		Dashboard.init(),
-		UserRoles.init()
-	]
-};
 
 // Initialize super tokens
 supertokens.init(SuperTokensConfig);
@@ -96,6 +24,7 @@ supertokens.init(SuperTokensConfig);
 const port: number = process.env.AUTHENTICATION_API_PORT as number | undefined ?? 3001;
 const host: string = '0.0.0.0';
 const app = express();
+const jsonParser = bodyParser.json();
 
 // Cors settings
 app.use(
@@ -114,6 +43,36 @@ app.use(middleware());
 app.get('/tenants', async (_req, res) => {
 	const tenants = await Multitenancy.listAllTenants();
 	res.send(tenants);
+});
+
+// Update user metadata with first and last name
+app.post('/user', verifySession(), jsonParser, async (req, res) => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const session = req.session;
+	const userId = session.getUserId();
+	await UserMetadata.updateUserMetadata(userId, {
+		// eslint-disable-next-line camelcase
+		first_name: req.body.first_name,
+		// eslint-disable-next-line camelcase
+		last_name: req.body.last_name
+	});
+	res.json({ message: 'Successfully updated user metadata' });
+});
+
+// Get user first and last name from metadata
+app.get('/user', verifySession(), async (req, res) => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const session = req.session;
+	const userId = session.getUserId();
+	const metadata = (await UserMetadata.getUserMetadata(userId)).metadata;
+	res.json({
+		// eslint-disable-next-line camelcase
+		first_name: metadata.first_name,
+		// eslint-disable-next-line camelcase
+		last_name: metadata.last_name
+	});
 });
 
 // Super Tokens error handling
